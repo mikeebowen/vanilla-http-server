d9e58d72b8584f983fb9d0f0d6f8e5a4b66d03b1
Michael Bowen <mikee.bowen@gmail.com>
2015-05-06 09:35:36 -0700

    make working test for greet function

--- test/server_test.js
+++ test/server_test.js
@@ -3,13 +3,13 @@
 var chai = require('chai');
 var expect = chai.expect;
 var chaiHttp = require('chai-http');
-require('../index.js');
 
 chai.use(chaiHttp);
+require('../index.js');
 
 describe('Server Check', function () {
   it('Should send a 404 error when entering a random link', function (done) {
-    chai.request('localhost:3000').get('/' + Math.random()).end(function (err, res) {
+    chai.request('localhost:3000').get('/blah'/* + Math.random()*/).end(function (err, res) {
       expect(err).to.eql(null);
       expect(res.status).to.eql(404);
       done();
@@ -17,8 +17,10 @@ describe('Server Check', function () {
   });
 
   it('Should show Hello Fred. How are you today?', function (done) {
-    chai.request('localhost:3000').get('/time'/* + 'Fred'*/).end(function (err, res) {
-      expect(err).to.eql(null);
+    chai.request('localhost:3000').get('/greet/Fred').end(function (err, res) {
+      // expect(err).to.eql(null);
+      console.log(res.body);
+      console.log(res.body.msg);
       expect(res.status).to.eql(200);
       expect(res.body.msg).to.eql('Hello Fred. How are you today?');
       done();
